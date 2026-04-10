33ff17699cb3e53b2761bbc2b7952b3ef27e4cc2
nulqqqqq <dlekm1@mail.ru>
2026-04-10 23:23:21 +0300

    fix: add default values for pagination to fix empty responses

--- GymManagement.Api/Dtos/Shared/PaginationQueryDto.cs
+++ GymManagement.Api/Dtos/Shared/PaginationQueryDto.cs
@@ -2,8 +2,8 @@ namespace GymManagement.Api.Dtos.Shared;
 
 public class PaginationQueryDto
 {
-    public int PageNumber { get; set; }
-    public int PageSize { get; set; }
+    public int PageNumber { get; set; } = 1;
+    public int PageSize { get; set; } = 10;
     public string? SortColumn { get; set; }
     public string? SortOrder { get; set; }
     
